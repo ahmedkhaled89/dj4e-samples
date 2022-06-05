0007192c7495891ca5adebfcef807e71abe13bde
ahmed <ahmedkhaled8901@gmail.com>
2022-06-05 20:02:13 +0200

    Just trying git commands

--- gview/views.py
+++ gview/views.py
@@ -4,7 +4,7 @@ from django.views import View
 from django.shortcuts import render
 
 # Create your views here.
-
+test = 'test'
 class CatListView(View):
     def get(self, request) :
         stuff = Cat.objects.all()
